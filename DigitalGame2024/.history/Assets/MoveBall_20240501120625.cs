a510d595a0e7e5eee7acf270b36fe78e427cd15e
Fodohl <108153274+Fodohl@users.noreply.github.com>
2024-05-02 12:00:55 +1200

    Player Controller

---
+++ DigitalGame2024/.history/Assets/MoveBall_20240501120625.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb.AddForce(new Vector3(0,height,0), ForceMode.Impulse);
+        }
+    }
+}
